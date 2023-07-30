62822607732d8e498f06b5e9ad762af9a3db6481
SU_VIN <pyotnqls25@naver.com>
2023-07-30 15:32:34 +0900

    라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
-  return <div className="App">page</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="Home">hi</div>;
+};
+
+export default Home;
