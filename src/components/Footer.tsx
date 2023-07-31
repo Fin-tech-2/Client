2031c60088a7c49c26c4bd79ba079f1347d219ec
SU_VIN <pyotnqls25@naver.com>
2023-07-31 14:46:10 +0900

    Footer 생성
    Footer 컴포넌트 만들어둠

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import "./../css/Footer.scss";
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div className="content">ⓒ핀테크 2조 아자아자 홧팅</div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import "./../css/Home.scss";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 const Home = () => {
   return (
     <div className="Home">
       <Header />
+      <Footer />
     </div>
   );
 };
