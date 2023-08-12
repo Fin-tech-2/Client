ac677ac9505bc0dad219163a2bfbfd652dbc8483
JuEun Kim <kje0405@naver.com>
2023-08-12 11:09:18 +0900

    마이페이지 이름, 이메일 연동 성공

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,8 +1,8 @@
-import React  from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from "../../components/Header";
 import "../../css/MyPage.scss";
-import {Tabs} from 'antd';
-import type {TabsProps} from 'antd';
+import { Tabs } from 'antd';
+import type { TabsProps } from 'antd';
 import InterestTab from "./InterestTab";
 import PurchaseDetailTab from "./PurchaseDetailTab";
 import MyLectureTab from "./MyLectureTab";
@@ -12,30 +12,51 @@ const onChange = (key: string) => {
 };
 
 const MyPage = () => {
+    const [userData, setUserData] = useState({
+        name: '',
+        email: '',
+        couponCount: 0
+    });
 
-    const items:TabsProps['items'] = [
-        {key: 'interest', label: `관심함`, children: <InterestTab />,},
-        {key: 'purchase-detail', label: `구매내역`, children: <PurchaseDetailTab />,},
-        {key: 'my-lecture', label: `내 강의`, children: <MyLectureTab />,},
+    useEffect(() => {
+        const storedName = sessionStorage.getItem('name');
+        const storedEmail = sessionStorage.getItem('email');
+
+        if (storedName && storedEmail) {
+            setUserData(prevData => ({
+                ...prevData,
+                name: storedName,
+                email: storedEmail,
+            }));
+        }
+    }, []);
+
+    const items: TabsProps['items'] = [
+        { key: 'interest', label: `관심함`, children: <InterestTab />, },
+        { key: 'purchase-detail', label: `구매내역`, children: <PurchaseDetailTab />, },
+        { key: 'my-lecture', label: `내 강의`, children: <MyLectureTab />, },
     ];
+
     return (
         <div>
-            <Header/>
+            <Header />
             <div className="profile">
                 <img
                     className="profileImage"
                     src={process.env.PUBLIC_URL + `/assets/profile.svg`}
+                    alt="Profile"
                 />
-                <div className="profileText"> {/* 이 부분 axios 형태로 코드 수정해야함 */}
-                    <h1> 홍길동 </h1>
-                    <p> hongkildong1234@fintech.com </p><br/>
-                    <p> 내 쿠폰: 0 </p>
+                <div className="profileText">
+                    <h1> {userData.name} </h1>
+                    <p> {userData.email} </p><br />
+                    <p> 내 쿠폰: {userData.couponCount} </p>
                 </div>
             </div>
             <div className="myPageTab">
-                <Tabs className="tab" defaultActiveKey="interest" items={items} onChange={onChange}/>
+                <Tabs className="tab" defaultActiveKey="interest" items={items} onChange={onChange} />
             </div>
         </div>
     );
 };
+
 export default MyPage;
