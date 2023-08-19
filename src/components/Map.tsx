8b9db42820c1a318882c05e6bf17ddda3a24da6b
SU_VIN <pyotnqls25@naver.com>
2023-08-19 13:04:17 +0900

    map api 추가
    1. google map api 추가

---
+++ src/components/Map.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useCallback } from "react";
+import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "500px",
+  height: "400px",
+};
+
+const center = {
+  lat: 37.546993,
+  lng: 126.950141,
+};
+
+interface ICenter {
+  lat: number;
+  lng: number;
+}
+
+function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBlviBcDqrPcymf7tTV0C-zERv-hK0EmSw", // 주의: 공개적인 장소에 API 키를 노출하지 마세요!
+  });
+
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [selectedMarker, setSelectedMarker] = useState<ICenter | null>(null);
+
+  const onLoad = useCallback((map: google.maps.Map) => {
+    const bounds = new window.google.maps.LatLngBounds(center as ICenter);
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(() => {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      <MarkerF
+        position={{ lat: 37.546993, lng: 126.950141 }}
+        icon={{
+          url: "/assets/Logo.svg",
+          scaledSize: new window.google.maps.Size(32, 32),
+        }}
+      />
+    </GoogleMap>
+  ) : null;
+}
+
+export default React.memo(MyComponent);
